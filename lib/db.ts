95c378ae7d53b465290c1519f3c7fd9c2207c399
yunchan123 <phenomenal312@hufs.ac.kr>
2024-07-08 15:16:03 +0900

    chapter 7

---
+++ lib/db.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from ".prisma/client";
+
+const db = new PrismaClient();
+
+// async function test() {
+//   const user = await db.user.create({
+//     data: {
+//       username: "admin2",
+//       phone: "12313123123",
+//     },
+//   });
+//   const users = await db.user.findMany({
+//     where: {
+//       username: {
+//         contains: "admin",
+//       },
+//     },
+//   });
+//   console.log(users);
+// }
+
+async function test() {
+  //   const token = await db.sMSToken.create({
+  //     data: {
+  //       token: "12312132",
+  //       user: {
+  //         connect: {
+  //           id: 1,
+  //         },
+  //       },
+  //     },
+  //   });
+  const token = await db.sMSToken.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      user: true,
+    },
+  });
+  console.log(token);
+}
+test();
+
+export default db;
